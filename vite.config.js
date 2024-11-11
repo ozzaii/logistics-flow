610b079b8f3477f51d650413ebb4eb487795898a
Kaan Özkan <168916101+ozzaii@users.noreply.github.com>
2024-11-11 18:53:33 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/logistics-flow/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
     rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
   },
 })
