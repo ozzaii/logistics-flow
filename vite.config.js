402cfd00d685652ee1a6bbbb705020047035aa9d
Kaan Özkan <168916101+ozzaii@users.noreply.github.com>
2024-11-11 18:24:14 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/logistics-flow/',  // Replace with your repo name
+  base: '/logistics-flow/'  // This should match your repo name
 })
