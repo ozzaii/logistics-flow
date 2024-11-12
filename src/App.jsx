365fe7fdecd6187c624afbcef643d4715a8c2b8c
Kaan Özkan <168916101+ozzaii@users.noreply.github.com>
2024-11-12 12:36:05 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Search } from 'lucide-react';
 import LogisticsDashboard from './components/LogisticsDashboard';
 
 // Updated API configuration
-const BASE_URL = "https://b8b1-34-143-163-90.ngrok-free.app";
+const BASE_URL = "https://ef9c-34-126-184-39.ngrok-free.app";
 const API_URL = `${BASE_URL}/predict`;
 
 const App = () => {
